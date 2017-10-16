6a6a329aedccad710da3ae2a8790c85d2a4db39a
Sathish <sathish.2009btech@gmail.com>
2017-10-17 01:16:39 +0530

    spring security service api

---
+++ modules/capehub-kernel/src/main/java/com/capestart/kernel/security/SecurityServiceSpringImpl.java
@@ -0,0 +1,33 @@
+package com.capestart.kernel.security;
+
+import com.capestart.security.api.Organization;
+import com.capestart.security.api.SecurityService;
+import com.capestart.security.api.User;
+
+public class SecurityServiceSpringImpl implements SecurityService {
+
+	@Override
+	public User getUser() throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Organization getOrganization() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setOrganization(Organization organization) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
